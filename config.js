fc703b69ffd91b5aabc8790994a306800b33af7f
zainmerchant50 <zainmerchant50@gmail.com>
2026-02-07 17:48:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/4LMlVCXHJtCE9abhmn0mYo", // Music streaming URL
+        musicUrl: "musicUrl: "Afusic - Pal Pal (Official Music Video) Prod. AliSoomroMusic.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
